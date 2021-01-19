31638dc7d7f722a927659d6cde42917c68885058
Bngjek <74656711+zaky-prog@users.noreply.github.com>
2021-01-20 03:19:53 +0700

    Add files via upload

---
+++ src/help.js
@@ -0,0 +1,232 @@
+const help = (prefix) => {
+	return `🔰 BOT ARA 🔰
+┏━━━━━━━━━━━━━━━━━━━━┓
+┃ *⚠️ NO SPAM! NO CALL!!! ⚠️*
+┃      *LANGGAR AUTO BLOCK*
+┣━━━━━━━━━━━━━━━━━━━━┛
+┃⊱❥ OWNER : 6285832079751
+┃⊱❥ YOUTUBE : gaada
+┃⊱❥ PREFIX : 「 ${prefix} 」
+┃⊱❥ VERSION : 1.0
+┣━━━━━━━━━━━━━━━━━━━━┛
+┃ INFO
+┣━━━━━━━━━━━━━━━━━━━━┓
+┃
+┃➢ ${prefix}info
+┃➢ ${prefix}donasi
+┃➢ ${prefix}blocklist
+┃➢ ${prefix}chatlist
+┃➢ ${prefix}ping
+┃➢ ${prefix}bugreport
+┃
+┣━━━━━━━━━━━━━━━━━━━━┛
+┃ MAKER
+┣━━━━━━━━━━━━━━━━━━━━┓
+┃
+┃➢ ${prefix}sticker
+┃➢ ${prefix}stickergif
+┃➢ ${prefix}toimg
+┃➢ ${prefix}tomp3
+┃➢ ${prefix}bpink
+┃➢ ${prefix}marvellogo
+┃➢ ${prefix}snowrite
+┃➢ ${prefix}3dtext
+┃➢ ${prefix}ninjalogo
+┃➢ ${prefix}water
+┃➢ ${prefix}firetext
+┃➢ ${prefix}logowolf
+┃➢ ${prefix}logowolf2
+┃➢ ${prefix}pornhub
+┃➢ ${prefix}glitch
+┃➢ ${prefix}neonlogo
+┃➢ ${prefix}neonlogo2
+┃➢ ${prefix}lionlogo
+┃➢ ${prefix}jokerlogo
+┃➢ ${prefix}shadow
+┃➢ ${prefix}burnpaper
+┃➢ ${prefix}coffee
+┃➢ ${prefix}lovepaper
+┃➢ ${prefix}woodblock
+┃➢ ${prefix}qowheart
+┃➢ ${prefix}mutgrass
+┃➢ ${prefix}undergocean
+┃➢ ${prefix}woodenboards
+┃➢ ${prefix}wolfmetal
+┃➢ ${prefix}metalictglow
+┃➢ ${prefix}8bit
+┃➢ ${prefix}ttp
+┃➢ ${prefix}quotemaker
+┃
+┣━━━━━━━━━━━━━━━━━━━━┛
+┃ SEARCH
+┣━━━━━━━━━━━━━━━━━━━━┓
+┃
+┃➢ ${prefix}trendtwit
+┃➢ ${prefix}randomkpop
+┃➢ ${prefix}ytsearch
+┃
+┣━━━━━━━━━━━━━━━━━━━━┛
+┃ QUOTEMAKER
+┣━━━━━━━━━━━━━━━━━━━━┓
+┃
+┃➢ ${prefix}wiki
+┃➢ ${prefix}wikien
+┃➢ ${prefix}nulis
+┃➢ ${prefix}quotes
+┃➢ ${prefix}quotes2
+┃➢ ${prefix}artinama
+┃
+┣━━━━━━━━━━━━━━━━━━━━┛
+┃ PRIMBON
+┣━━━━━━━━━━━━━━━━━━━━┓
+┃
+┃➢ ${prefix}apakah
+┃➢ ${prefix}kapankah
+┃➢ ${prefix}rate
+┃➢ ${prefix}bisakah
+┃➢ ${prefix}alay
+┃➢ ${prefix}gantengcek
+┃➢ ${prefix}watak
+┃➢ ${prefix}hobby
+┃➢ ${prefix}game
+┃➢ ${prefix}bucin
+┃➢ ${prefix}trust
+┃➢ ${prefix}dare
+┃➢ ${prefix}simi
+┃
+┣━━━━━━━━━━━━━━━━━━━━┛
+┃ DOWNLOAD 
+┣━━━━━━━━━━━━━━━━━━━━┓
+┃
+┃➢ ${prefix}pinterest
+┃➢ ${prefix}ytmp3
+┃➢ ${prefix}ytmp4
+┃➢ ${prefix}tiktok
+┃➢ ${prefix}joox
+┃
+┣━━━━━━━━━━━━━━━━━━━━┛
+┃ FUN
+┣━━━━━━━━━━━━━━━━━━━━┓
+┃
+┃➢ ${prefix}meme
+┃➢ ${prefix}memeindo
+┃
+┣━━━━━━━━━━━━━━━━━━━━┛
+┃ GRUB
+┣━━━━━━━━━━━━━━━━━━━━┓
+┃
+┃➢ ${prefix}opengc
+┃➢ ${prefix}closegc
+┃➢ ${prefix}promote
+┃➢ ${prefix}demote
+┃➢ ${prefix}tagall
+┃➢ ${prefix}tagall2
+┃➢ ${prefix}tagall3
+┃➢ ${prefix}tagall4
+┃➢ ${prefix}tagall5
+┃➢ ${prefix}add
+┃➢ ${prefix}kick
+┃➢ ${prefix}listadmins
+┃➢ ${prefix}linkgroup
+┃➢ ${prefix}leave
+┃➢ ${prefix}welcome
+┃➢ ${prefix}nsfw
+┃➢ ${prefix}leveling
+┃➢ ${prefix}level
+┃➢ ${prefix}delete
+┃➢ ${prefix}simih
+┃➢ ${prefix}ownergroup
+┃
+┣━━━━━━━━━━━━━━━━━━━━┛
+┃ FUN2
+┣━━━━━━━━━━━━━━━━━━━━┓
+┃
+┃➢ ${prefix}play
+┃➢ ${prefix}tts
+┃
+┣━━━━━━━━━━━━━━━━━━━━┛
+┃ MEDIA
+┣━━━━━━━━━━━━━━━━━━━━┓
+┃
+┃➢ ${prefix}lirik
+┃➢ ${prefix}chord
+┃
+┣━━━━━━━━━━━━━━━━━━━━┛
+┃ ISLAM
+┣━━━━━━━━━━━━━━━━━━━━┓
+┃
+┃➢ ${prefix}quran
+┃
+┣━━━━━━━━━━━━━━━━━━━━┛
+┃ FUN3
+┣━━━━━━━━━━━━━━━━━━━━┓
+┃
+┃➢ ${prefix}tiktokstalk
+┃➢ ${prefix}igstalk
+┃
+┣━━━━━━━━━━━━━━━━━━━━┛
+┃ WIBU
+┣━━━━━━━━━━━━━━━━━━━━┓
+┃
+┃➢ ${prefix}neonime
+┃➢ ${prefix}pokemon
+┃➢ ${prefix}loli
+┃➢ ${prefix}waifu
+┃➢ ${prefix}randomanime
+┃➢ ${prefix}husbu
+┃➢ ${prefix}husbu2
+┃➢ ${prefix}wait
+┃➢ ${prefix}nekonime
+┃
+┣━━━━━━━━━━━━━━━━━━━━┛
+┃ WIBU2
+┣━━━━━━━━━━━━━━━━━━━━┓
+┃
+┃➢ ${prefix}randomhentai
+┃➢ ${prefix}nsfwtrap
+┃➢ ${prefix}nsfwneko
+┃
+┣━━━━━━━━━━━━━━━━━━━━┛
+┃ BAHASA
+┣━━━━━━━━━━━━━━━━━━━━┓
+┃
+┃➢ ${prefix}bahasa
+┃➢ ${prefix}kodenegara
+┃➢ ${prefix}kbbi
+┃➢ ${prefix}fakta
+┃➢ ${prefix}infocuaca
+┃➢ ${prefix}infogempa
+┃➢ ${prefix}jadwaltv
+┃➢ ${prefix}covid
+┃
+┣━━━━━━━━━━━━━━━━━━━━┛
+┃ OWNER
+┣━━━━━━━━━━━━━━━━━━━━┓
+┃
+┃➢ ${prefix}setprefix
+┃➢ ${prefix}block
+┃➢ ${prefix}bc
+┃➢ ${prefix}bcgc
+┃➢ ${prefix}clone
+┃➢ ${prefix}clearall
+┃
+┣━━━━━━━━━━━━━━━━━━━━┛
+┃ LAIN2
+┣━━━━━━━━━━━━━━━━━━━━┓
+┃
+┃➢ ${prefix}send
+┃➢ ${prefix}wame
+┃➢ ${prefix}virtex
+┃➢ ${prefix}exe
+┃➢ ${prefix}qrcode
+┃➢ ${prefix}afk
+┃➢ ${prefix}fml
+┃➢ ${prefix}fml2
+┃
+┣━━━━━━━━━━━━━━━━━━━━┛
+┃⟪ MADE BY ARA BOT ⟫
+┣━━━━━━━━━━━━━━━━━━━━┓
+`
+}
+
+exports.help = help
